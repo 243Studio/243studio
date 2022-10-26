2183dc7b9cdfa8119dd0d4af9eb29fec26433fcb
LeGrandMAG <magmukendi0@gmail.com>
2022-10-26 21:19:46 +0900

    added a gh-pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/App.css';
 import { Projects } from './Data/project';
-import {useEffect, useRef} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import useMouse from '@react-hook/mouse-position'
+import Details from './components/Details'
+let globalIndex = 0;
+
+
 
 function App() {
 
-  const ref = useRef(null)
+  const [pos, setPos] = useState({x:0, y:0})
+  const [display, setDisplay] = useState(false)
+  const [detailTitle, setDetailTitle] = useState("")
+  const [detailLink, setDetailLink] = useState("")
 
+  const ref = useRef(null)
   const mouse = useMouse(ref, {
     enterDelay: 100,
     leaveDelay: 100,
   })
 
+  let last = {x:0, y:0 };
+
+
+let t = 0
+let state = "on"
+function activate(image, x, y) {
+  
+  let coco = document.getElementById(image)
+  coco.style.zIndex = t
+  coco.classList.add('show')
+  //console.log(coco)
+  coco.style.left = `${x}px`;
+  coco.style.top = `${y}px`;
+
+  /*const r = document.getElementsByTagName("img")
+  for (let t = 0; t< r.length; t++){
+    r[t].style.zIndex = 0
+  } */
+  //const p = r.map(item => item.style.zIndex = 0)
+  //image.dataset.status = "active";
+  last = { x, y }
+  
+}
+
+
+const distanceFromLast = (x,y) =>{
+  return Math.hypot(x - parseInt(last.x), y - parseInt(last.y))
+}
+
+//controlMouse(e)
+
+  //console.log(e)
+  
+useEffect(()=>{
+  //console.log(pos.x, pos.y)
+  controlMouse()
+},[pos.x, pos.y])
+  function position(e){
+      setPos((prev)=>{
+        
+        
+        if(Math.hypot(e.clientX - prev.x, e.clientY - prev.y) > 100 ){
+          pos.x = e.clientX
+          pos.y = e.clientY
+          return pos
+        }
+
+        else{
+          return {...prev}
+        }
+      })
+
+  }
+function controlMouse(){
+  if(distanceFromLast(mouse.x,mouse.y) > 100){
+    const lead = Projects[globalIndex % Projects.length]['name'];
+    //const tail = Projects[(globalIndex - 5) % Projects.length]['name']
+    //console.log(lead)
+    //console.log(mouse.x, mouse.y)
+    activate(lead, pos.x, pos.y);
+    /*if(tail) {
+      tail.dataset.status = "inactive"
+    }*/
+    
+    
+    
+    for (let i = 0; i < Projects.length; i++){
+      //console.log((globalIndex + i)  % Projects.length )
+      //images[(globalIndex  % images.length) - i ].style.zIndex = 10 - i
+    }
+    
+    //console.log(lead.style.zIndex)
+    //console.log(globalIndex++)
+    globalIndex++;
+    t ++;
+  };
+} 
+  function changeDisplay(){
+      setDisplay(false)
+  }
+  function showDetails(project){
+    setDisplay(true)
+    setDetailTitle(project.name)
+    setDetailLink(project.link)
+    let x = document.querySelectorAll("img")
+    for (let i=0; i< x.length; i++){
+      x[i].classList.remove("show")
+    }
+  }
+  
+ 
   const Images = Projects.map(image =>{
     let imageId = Projects.indexOf(image)
-    return(
 
-      <img className='show' id={imageId} style={{width:'100px', top:mouse.y- imageId*5, left:mouse.x-imageId*5}} key = {image.link} src= {image.link} />
-    )
+      let x = `${mouse.x}`
+      let y = `${mouse.y}`
+      return(
+        
+        
+        mouse.x!==null && mouse.y!==null && <a  onClick={()=>showDetails(image)}>
+          <img className='' alt={image.name} id={image.name}  key = {image.link} src= {image.link} /> 
+          </a>
+        
+        )
+
   }
   )
   return (
-    <div ref={ref} className="App">
-      <div style={{top:mouse.y, left:mouse.x}} className='cursor'></div>
+    <div onMouseMove={position} ref={ref} className="App">
 
-      {/*<p>
-        x: {mouse.x} 
-        y: {mouse.y}
-  </p>*/}
-      {Images}
+
+      {display && <Details 
+      name={detailTitle}
+      link={detailLink}
+      changeDisplay={changeDisplay}
+      x={mouse.x}
+      y={mouse.y}
+      />
+      
+      
+      //mouse.x!==null && mouse.y!==null && <div style={{top:mouse.y, left:mouse.x}} className='cursor'></div> 
+      }
+
+      <p className="ref">    x: {pos.x} y: {pos.y}
+  </p>
+      {Images
+      }
+     
     </div>
   );
 }
--- src/Data/project.js
+++ src/Data/project.js
@@ -1,36 +1,48 @@
 
 export  const Projects =[
     {
+        name:"243 Studio",
         link: '/images/243STUDIO.jpg'
     },
     {
+        name:"WEPO RDC",
         link: '/images/WEPO.png'
     },
     {
+        name:"CARP RDC",
         link: '/images/CARP.png'
     },
     {
+        name:"CARP RDC",
         link: "/images/x2.png"
     },
     {
-        link: "/images/x1.png"
+        name:"CARP RDC",
+        link: "/images/x1.png",
+        
     },
     {
+        name:"Culdi",
         link: "/images/aldadin.png"
     },
     {
+        name:"Toli Bleu",
         link:"images/tolibleu.png"
     },
     {
+        name:"Congo Independance",
         link:"images/independance.png"
     },
     {
+        name:"Culdi HUFS",
         link:"images/Culdi.png"
     },
     {
+        name:"Congo Excellence",
         link:"images/Congo.png"
     },
     {
+        name:"Christmas",
         link:"/images/christmas.png"
     },
     
